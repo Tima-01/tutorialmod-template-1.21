f3d54f5d0fe719e5e497873676e7c0480329ccd2
Tima-01 <gmod1600@mail.ru>
2026-03-17 21:42:38 +0600

    added Blocks

--- src/main/java/zaleskar/tutorial/Tutorialmod.java
+++ src/main/java/zaleskar/tutorial/Tutorialmod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import zaleskar.tutorial.block.ModBlocks;
 import zaleskar.tutorial.item.ModItems;
 
 public class Tutorialmod implements ModInitializer {
@@ -13,5 +14,6 @@ public class Tutorialmod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
